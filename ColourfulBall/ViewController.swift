c87fc00abcf0381671b71cf619cee2a92db18f5c
Nguyen Thuong Thong <nguyenthuongthongcm@gmail.com>
2018-05-25 15:36:33 +0700

    Add RxSwift BackgroundColor

--- ColourfulBall/ViewController.swift
+++ ColourfulBall/ViewController.swift
@@ -6,20 +6,89 @@
 //  Copyright © 2018 ninjaKID. All rights reserved.
 //
 
+import ChameleonFramework
 import UIKit
+import RxSwift
+import RxCocoa
+
 
 class ViewController: UIViewController {
 
+    var circleView: UIView!
+    var circleViewModel = CircleViewModel()
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setup()
+    }
+    
+    func setup() {
+        // Add circle view
+        circleView = UIView(frame: CGRect(origin: view.center, size: CGSize(width: 100.0, height: 100.0)))
+        circleView.layer.cornerRadius = circleView.frame.width / 2.0
+        circleView.center = view.center
+        circleView.backgroundColor = .green
+        view.addSubview(circleView)
+        
+        // Add gesture recognizer
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(circleMoved(_:)))
+        circleView.addGestureRecognizer(gestureRecognizer)
+        
+        // Bind the center point of the CircleView to the centerObservable
+        circleView
+            .rx.observe(CGPoint.self, "center")
+            .bind(to: circleViewModel.centerVariable)
+            .disposed(by: disposeBag)
+        
+        // Subcribe to backgroundObservable to get new colors from the ViewModel
+        circleViewModel.backgroundColorObservable
+            .subscribe(onNext: { [weak self] backgroundColor in
+                UIView.animate(withDuration: 0.1, animations: {
+                    self?.circleView.backgroundColor = backgroundColor
+                    // Try to get complementary color for given background color
+                    let viewBackgroundColor = UIColor(complementaryFlatColorOf: backgroundColor)
+                    // If it is different that the color
+                    if viewBackgroundColor != backgroundColor {
+                        // Assign it as a background color of the view
+                        // We only want different color to be able to see that circle in a view
+                        self?.view.backgroundColor = viewBackgroundColor
+                    }
+                    
+                })
+               
+            }).disposed(by: disposeBag)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func circleMoved(_ recognizer: UIPanGestureRecognizer) {
+        let location = recognizer.location(in: view)
+        UIView.animate(withDuration: 0.1) {
+            self.circleView.center = location
+        }
     }
-
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
